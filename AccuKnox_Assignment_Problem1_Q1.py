0b2e45a6bca7bd77651d63dce1d4397da094948c
Jordan D'Souza <121935089+jordan-dsouza@users.noreply.github.com>
2026-01-13 14:14:37 +0530

    Update AccuKnox_Assignment_Problem1_Q1.py
    Edited spaces.

--- AccuKnox_Assignment_Problem1_Q1.py
+++ AccuKnox_Assignment_Problem1_Q1.py
@@ -28,39 +28,40 @@ year INTEGER
 	conn.commit()		# Permanently saves changes
 	conn.close()		# Terminate database connection
 	
-	# - - - Store Data in DB - - -
+# - - - Store Data in DB - - -
 	
-	def store_books(books):
-		conn = sqlite3.connect(DB_NAME)
-		cursor = conn.cursor()
+def store_books(books):
+	conn = sqlite3.connect(DB_NAME)
+	cursor = conn.cursor()
 		
-		for book in books:
-			cursor.execute("""
+	for book in books:
+		cursor.execute("""
 				INSERT INTO books (title, author, year)
 				VALUES (?, ?, ?)""",
 				(book["title"], book["author"], book["year"])
 				)
 
-		conn.commit()
-		conn.close()
+	conn.commit()
+	conn.close()
 
-	# - - - 3. Retrieve and Display Data - - -
+# - - - 3. Retrieve and Display Data - - -
 	
-	def display_books():
-		conn = sqlite3.connect(DB_NAME)
-		cursor = conn.cursor()
+def display_books():
+	conn = sqlite3.connect(DB_NAME)
+	cursor = conn.cursor()
 		
-		cursor.execute(" SELECT title, author, year FROM books")
-		rows = cursor.fetchall()		# Retrieve all rows from db
+	cursor.execute(" SELECT title, author, year FROM books")
+	rows = cursor.fetchall()		# Retrieve all rows from db
 		
-		print("\n Stored Books:")
-		for title, author, year in rows:
-			print("f - {title} by {author} ({year})")
+	print("\n Stored Books:")
+	for title, author, year in rows:
+		print("f - {title} by {author} ({year})")
 
-		conn.close()		# Not using commit as no changes made to database
+	conn.close()		# Not using commit as no changes made to database
 	
-	if __name__ == "__main__":
-		init_db()
-		books = fetch_books()
-		store_books(books)
+if __name__ == "__main__":
+	init_db()
+	books = fetch_books()
+	store_books(books)
+
 		display_books()
